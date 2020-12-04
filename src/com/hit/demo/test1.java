38ac5c8cf04103d6831c37b4dfe0413a2006e2fe
sxin-0617 <1357090984@qq.com>
2020-12-04 16:26:53 +0800

    Initial commit

---
+++ src/com/hit/demo/test1.java
@@ -0,0 +1,8 @@
+package com.hit.demo;
+/* created by hezixin on 2020/12/4 */
+
+public class test1 {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
